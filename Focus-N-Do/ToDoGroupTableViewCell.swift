051e9a421290536f92204a0e8e095a000c092023
darkestmidnight <elly.richardson18@gmail.com>
2019-07-14 00:03:36 -0500

    Added some stuff in the new tableViewCell

--- Focus-N-Do/ToDoGroupTableViewCell.swift
+++ Focus-N-Do/ToDoGroupTableViewCell.swift
@@ -8,17 +8,37 @@
 
 import UIKit
 
-class ToDoGroupTableViewCell: UITableViewCell {
+class ToDoGroupTableViewCell: UITableViewCell, UITableViewDataSource, UITableViewDelegate{
+    var toDos = [ToDo]()
+    var toDoSubMenuTable: UITableView?
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        setupSubTable()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    // MARK: - Table View Data Source
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    // MARK: - Private Methods
+    private func setupSubTable() {
+        toDoSubMenuTable = UITableView(frame: CGRect.zero, style: UITableView.Style.plain)
+        toDoSubMenuTable?.delegate = self
+        toDoSubMenuTable?.dataSource = self
+        self.addSubview(toDoSubMenuTable!)
     }
 
 }
